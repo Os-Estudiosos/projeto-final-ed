8ce823489779aa83fa0a773fe8833e49ded0cf6f
lucasbp6 <lucasbatistapereira06@gmail.com>
2025-05-31 18:23:42 -0300

    tree_utils - declaracao

---
+++ src/tree_utils.cpp
@@ -0,0 +1,5 @@
+#include "tree_utils.h"
+
+int main(){
+    
+}
---
+++ src/tree_utils.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+struct Node {
+    std::string word;
+    std::vector<int> documentIds;
+    Node* parent;
+    Node* left;
+    Node* right;
+    int height;    // usado na AVL
+    int isRed;     // usado na RBT
+};
+
+struct BinaryTree {
+    Node* root;
+    Node* NIL;  // usado na RBT (Opcional)
+};
+
+void printIndex(BinaryTree* tree);
+void printTree(BinaryTree* tree);
