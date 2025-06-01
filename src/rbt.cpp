952eaecadd03fde183902d15b84720d120e28f9e
matheuspinto4 <mat.vsp@gmail.com>
2025-06-01 15:55:38 -0300

    Adicionei comentários e docstring

--- src/rbt.cpp
+++ src/rbt.cpp
@@ -7,34 +7,25 @@ namespace RBT
         Node* y = x->right;
         x->right = y->left;
         
-        if(y->left != nullptr)
-        {
+        if (y->left != nullptr)
             y->left->parent = x;
-        }
 
         y->parent = x->parent;
 
+        // Atualiza pai de x
         if (x->parent == nullptr)
-        {
             *root = y;
-        }
         else if (x == x->parent->left)
-        {
             x->parent->left = y;
-        }
         else
-        {
             x->parent->right = y;
-        }
 
         y->left = x;
         x->parent = y;
-
     }
 
-
     void rotateRight(Node** root, Node* y)
-    {   
+    {
         Node* x = y->left;
         y->left = x->right;
 
@@ -43,6 +34,7 @@ namespace RBT
 
         x->parent = y->parent;
 
+        // Atualiza pai de y
         if (y->parent == nullptr)
             *root = x;
         else if (y == y->parent->right)
@@ -54,62 +46,63 @@ namespace RBT
         y->parent = x;
     }
 
-    
     void fixInsert(Node **root, Node* z)
     {
-        while (z->parent != nullptr && z->parent->isRed == RED) 
+        while (z->parent != nullptr && z->parent->isRed == RED)
         {
             Node* parent = z->parent;
-            Node* grandparent = z->parent->parent;
-            
-            // pai é filho da esquerda
-            if (parent == grandparent->left) 
-            { 
-                Node* uncle = grandparent->right; 
-                
-                // Caso 1: pai e tio são vermelhos 
-                if (uncle->isRed == RED) 
+            Node* grandparent = parent->parent;
+
+            if (parent == grandparent->left)
+            {
+                Node* uncle = grandparent->right;
+
+                if (uncle != nullptr && uncle->isRed == RED)
                 {
+                    // Tio vermelho — recoloração
                     parent->isRed = BLACK;
                     uncle->isRed = BLACK;
                     grandparent->isRed = RED;
                     z = grandparent;
-                } else {
-                    // Caso 2: z é filho à direita 
-                    if (z == parent->right) 
+                }
+                else
+                {
+                    if (z == parent->right)
                     {
+                        // Conversão para caso 3
                         z = parent;
                         rotateLeft(root, z);
                         parent = z->parent;
                         grandparent = parent->parent;
                     }
-                    // Caso 3: z é filho à esquerda 
+
+                    // Corrige com rotação
                     parent->isRed = BLACK;
                     grandparent->isRed = RED;
                     rotateRight(root, grandparent);
                 }
-            } else 
+            }
+            else
             {
-                // Casos simétricos (pai é filho da direita)
                 Node* uncle = grandparent->left;
 
-                // Caso 1
-                if (uncle != nullptr && uncle->isRed == RED) 
+                if (uncle != nullptr && uncle->isRed == RED)
                 {
                     parent->isRed = BLACK;
                     uncle->isRed = BLACK;
                     grandparent->isRed = RED;
                     z = grandparent;
-                } else {
-                    // Caso 2
-                    if (z == parent->left) 
+                }
+                else
+                {
+                    if (z == parent->left)
                     {
                         z = parent;
                         rotateRight(root, z);
                         parent = z->parent;
                         grandparent = parent->parent;
                     }
-                    // Caso 3
+
                     parent->isRed = BLACK;
                     grandparent->isRed = RED;
                     rotateLeft(root, grandparent);
@@ -117,8 +110,6 @@ namespace RBT
             }
         }
 
-        (*root)->isRed = BLACK; // Garante que a raiz é preta 
-
+        (*root)->isRed = BLACK; // Raiz deve ser preta
     }
 }
-
