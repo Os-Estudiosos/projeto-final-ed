f40923d18b82634dc1188e70bceca680d093229d
ThalisAFalqueto <thalisambrosimfalqueto@gmail.com>
2025-06-01 22:54:00 -0300

    apaguei uma função não útil de criar nó(não funcionava pois teria que alterar o vetor de ids)

--- src/tree_utils.cpp
+++ src/tree_utils.cpp
@@ -9,20 +9,6 @@ void printTree(BinaryTree *tree)
     // implementar
 }
 
-Node *createNode(std::string x)
-{
-    Node *node = new Node();
-    node->documentIds = std::vector<int>{};
-    node->word = x;
-    node->left = nullptr;
-    node->right = nullptr;
-    node->parent = nullptr;
-    node->height = 0;
-    node->isRed = 0;
-
-    return node;
-}
-
 void deleteNode(Node *node)
 {
     if (node == nullptr)
